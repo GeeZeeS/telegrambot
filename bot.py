ae37c85a64f559eec6d546d253639165adf5c53b
Admin <roundycube@gmail.com>
2019-07-26 12:02:57 +0300

    added requirements.txt

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import telebot
 import time
 import requests
 
-bot_token = 'your_bot_token'
+bot_token = '806161922:AAFvf9OgwfJMvfamBtEL4Ve1c9_HwAhaxWc'
 
 bot = telebot.TeleBot(token = bot_token)
 
